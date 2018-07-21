ed1e37390e395943ff23f26d3ba9752348f2be86
ryuconf <32991963+ryuconf@users.noreply.github.com>
2018-07-21 19:07:36 +0800

    Add Domains

---
+++ build.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+var src_path = path.normalize(__dirname + '/src');
+var dist_path = path.normalize(__dirname + '/dist');
+
+/* 获取文件 */
+var entris = fs.readdirSync(src_path).reduce( function(o, filename) {
+    if( /\.txt$/.test(filename)){
+        o[filename] = {
+            srcPath: path.join(src_path, filename),
+            distPath: path.join(dist_path, filename)
+        };
+    }
+    return o;
+}, {});
+
+
+/* 加密 */
+function base64_encode(file, filename) {
+    // read binary data
+    var bitmap = fs.readFileSync(file, 'utf-8');
+
+    // convert binary data to base64 encoded string
+    var base64Str = new Buffer(bitmap).toString('base64');
+
+    fs.writeFileSync(filename, base64Str);
+}
+
+for(var v in entris){
+    var file = entris[v];
+    base64_encode(file.srcPath, file.distPath);
+}
---
+++ restore.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+var src_path = path.normalize(__dirname + '/dist');
+var dist_path = path.normalize(__dirname + '/src');
+
+/* 获取文件 */
+var entris = fs.readdirSync(src_path).reduce( function(o, filename) {
+    if( /\.txt$/.test(filename)){
+        o[filename] = {
+            srcPath: path.join(src_path, filename),
+            distPath: path.join(dist_path, filename)
+        };
+    }
+    return o;
+}, {});
+
+/* 创建目录 */
+if (!fs.existsSync(dist_path)) {
+    fs.mkdirSync(dist_path, '0777', true);
+}
+
+/* 加密 */
+function base64_decode(file, filename) {
+    // read binary data
+    var bitmap = fs.readFileSync(file, 'utf-8');
+
+    // convert binary data to ascii encoded string
+    var asciiStr = new Buffer(bitmap, 'base64').toString('utf8');
+
+    fs.writeFileSync(filename, asciiStr);
+}
+
+for(var v in entris){
+    var file = entris[v];
+    base64_decode(file.srcPath, file.distPath);
+}
